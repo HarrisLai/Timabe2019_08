10b2a388316ac15a01156e7eedf73a4a39a5d83c
HarrisLai <harrislai33@gmail.com>
2019-08-23 23:30:54 +0800

    Add files via upload
    backup matlab.py plot sample

---
+++ 2019_08_16_視窗程式設計/matlab.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+age = [10,20,30,40,50]
+values=[0,1,2,3,4]
+
+
+#plt.xlable("age")
+#ply.ylable("number")
+#plt.title("test gram")
+
+plt.plot(age, value)
+plt.show()
+
+
---
+++ 2019_08_16_視窗程式設計/numpy.py
@@ -0,0 +1,6 @@
+import numpy
+
+#np_height = np.array(hight)
+
+n = numpy.array([1])
+#np_weight = np.array(weight)
